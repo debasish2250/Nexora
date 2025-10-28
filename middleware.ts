367d58390212e929b52652079656750a28bab3bc
Debasish Rout <debasishrout225@gmail.com>
2025-10-29 03:06:05 +0530

    middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,20 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"]
+  // Add the homepage "/" to your public routes
+  publicRoutes: ["/", "/api/uploadthing"] 
 });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
 };
+
+/*import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: ["/api/uploadthing"]
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
+};*/
