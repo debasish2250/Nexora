047bb8d464072693c74642dec743c7a008b380c2
Debasish Rout <debasishrout225@gmail.com>
2025-10-29 03:10:38 +0530

    Nexora

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // Add the homepage "/" to your public routes
-  publicRoutes: ["/", "/api/uploadthing"] 
+  publicRoutes: ["/api/uploadthing"]
 });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
 };
 
-/*import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"]
-});
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
-};*/
